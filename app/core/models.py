39520d577f5b803af5b984c3af237bd9a2ce7aca
unknown <tranminhnhan271099@gmail.com>
2023-01-22 00:45:45 +0700

    fix linting issue

--- app/core/models.py
+++ app/core/models.py
@@ -2,40 +2,38 @@
 Database models
 """
 
-from django.db import models  
+from django.db import models
 from django.contrib.auth.models import (
-	AbstractBaseUser,
-	BaseUserManager,
-	PermissionsMixin
-	)
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+    )
 
 
 class UserManager(BaseUserManager):
-	"""
-	Mangager for users
-	"""
-	def create_user(self, email, password=None, **extra_fields):
-		"""
-		Create, save and return a new user
-		"""
-		user = self.model(email=email, **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
+    """
+    Mangager for users
+    """
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create, save and return a new user
+        """
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-		return user
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	"""
-	User in the systems
-	"""
-	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+    """
+    User in the systems
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-	objects = UserManager()
+    objects = UserManager()
 
-	USERNAME_FIELD = 'email'
-
-
+    USERNAME_FIELD = 'email'
